9e7b77bc40a1cb0ce4f60e0733f15d8e8df76d40
Chris Davis <chrisdavis132@gmail.com>
2013-08-03 15:18:44 +0200

    Add times

--- Original/examples/main-serial.cpp
+++ Original/examples/main-serial.cpp
@@ -5,40 +5,77 @@
 #include "../LinearAlgebra/LinAlgHeader.hpp"
 #include "../Fem/FemHeader.hpp"
 #include "functions.hpp"
+#include "timer.h"
 
 using namespace std;
 
 
 int main(int argc, char *argv[]){
-    if (argc!=2) {
-        cerr << "usage: " << argv[0] << "  example_directory" << endl;
+
+	Timer timer;
+	timer.start();
+	
+	/* *** check input parameters */
+    if (argc!=5) {
+        cerr << "usage: " << argv[0] << "  example_directory, #refinements, solver, timings" << endl;
         exit(-1);
     }
     char directory [256]  = "";
     strcat(directory,argv[1]);
+    
+    int numRefine = atoi(argv[2]);
+    bool timings =  atoi(argv[4]);
+    
+    /* *** parse solver input */
+    Solver solver;
+    string chosen_solver = argv[3];
+     if (chosen_solver == "cg" || chosen_solver == "CG") {
+    
+    	solver = cg;
+    	
+    }
+    else if (chosen_solver == "gs" || chosen_solver == "GS") {
+    
+    	solver = gs;
+    	
+    }
+    else {
+    
+    	cerr << "Invalid solver specified!" << endl;
+    	exit(-1);
+    	
+    }    
 
     /*** read the input data */
 	IndexMatrix elements, dirichlet, neumann;
 	Matrix coordinates;
 	readMesh(argv[1],coordinates, elements, dirichlet, neumann);
 	
-	/* *** create mesh */
+	timer.outNoMPI("load", timings);
+	
+	/* *** create and refine mesh */
 	Mesh mesh(coordinates, elements, dirichlet,neumann);
+	
+	/* *** refine */
+    for (int i=0; i<numRefine; ++i) {
+	   	mesh.refineRed();
+	}
+	
+	timer.outNoMPI("mesh", timings);
   
     /* *** create fem object and assemble linear system */
-    FEM fem(mesh, f, DirichletData,NeumannData);
+    FEM fem(mesh, f, DirichletData, NeumannData);
+
     fem.assemble();
-  
-    /* *** write Galerkin matrix */
-    CRSMatrix A = fem.getA();
-    A.writeFull("./output/A_serial.txt");
+    
+    timer.outNoMPI("assembly", timings);
 	
-	DataVector b = fem.getb();
-	b.writeData(0,"./output/b_serial");
-	
-	/* *** solve problem using the cg method */
-    fem.solve(cg);
-	
+	/* *** solve problem using specified method */
+    fem.solve(solver);
+    
+    timer.outNoMPI("solving", timings);
+
+	/* *** write solution to file */
 	fem.writeSolution();
 	
 	
